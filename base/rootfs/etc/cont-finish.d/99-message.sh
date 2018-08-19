1902ff1f3cbe6a17e7baf072c664c8c544028fb2
Franck Nijhof <frenck@addons.community>
2018-08-19 08:32:08 +0200

    :ambulance: Reverses Systeminfo in crash message

--- base/rootfs/etc/cont-finish.d/99-message.sh
+++ base/rootfs/etc/cont-finish.d/99-message.sh
@@ -3,25 +3,8 @@
 # Community Hass.io Add-ons: Base Images
 # Displays an message right before terminating in case something went wrong
 # ==============================================================================
-# shellcheck source=base/rootfs/usr/lib/hassio-addons/base.sh
-source /usr/lib/hassio-addons/base.sh
-
-declare HASSIO_TOKEN
 
 if [[ "${S6_STAGE2_EXITED}" -ne 0 ]]; then
-
-    if hass.api.supervisor.ping; then
-        # shellcheck disable=SC2034
-        HASSIO_TOKEN=$(</var/run/s6/container_environment/HASSIO_TOKEN)
-        echo '-----------------------------------------------------------'
-        echo -n " v$(hass.addon.version)" && \
-        echo -n " / $(hass.api.supervisor.info.arch)" && \
-        echo -n " / $(hass.api.host.info.operating_system)" && \
-        echo -n " / HA $(hass.api.homeassistant.info.version)" && \
-        echo -n " / SU $(hass.api.supervisor.info.version)"
-        echo " / $(hass.api.supervisor.info.channel)"
-    fi
-
     echo '-----------------------------------------------------------'
     echo '                Oops! Something went wrong.'
     echo ' '
