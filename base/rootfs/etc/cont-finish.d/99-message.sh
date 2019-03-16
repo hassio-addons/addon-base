e7ecbf7849fb4598e8031af8116f8750c819583e
Franck Nijhof <frenck@frenck.nl>
2019-03-16 17:43:24 +0100

    V3.0.0 (#35)
    * :arrow_up: Upgrades Alpine to 3.9.2
    
    * :arrow_up: Upgrades curl to 7.64.0-r1
    
    * :arrow_up: Upgrades jq to 1.6-r0
    
    * :arrow_up: Upgrades tzdata to 2018i-r0
    
    * :fire: Removes obsolete packages
    
    * :sparkles: Adds support for ARMv7
    
    * :sparkles: Adds Bashio

--- base/rootfs/etc/cont-finish.d/99-message.sh
+++ base/rootfs/etc/cont-finish.d/99-message.sh
@@ -1,16 +1,17 @@
-#!/bin/bash
+#!/usr/bin/env bashio
 # ==============================================================================
 # Community Hass.io Add-ons: Base Images
 # Displays an message right before terminating in case something went wrong
 # ==============================================================================
-
 if [[ "${S6_STAGE2_EXITED}" -ne 0 ]]; then
-    echo '-----------------------------------------------------------'
-    echo '                Oops! Something went wrong.'
-    echo ' '
-    echo ' We are so sorry, but something went terribly wrong when'
-    echo ' starting or running this add-on.'
-    echo ' '
-    echo ' Be sure to check the log above, line by line, for hints.'
-    echo '-----------------------------------------------------------'
+    bashio::log.red \
+        '-----------------------------------------------------------'
+    bashio::log.red '                Oops! Something went wrong.'
+    bashio::log.red
+    bashio::log.red ' We are so sorry, but something went terribly wrong when'
+    bashio::log.red ' starting or running this add-on.'
+    bashio::log.red ' '
+    bashio::log.red ' Be sure to check the log above, line by line, for hints.'
+    bashio::log.red \
+        '-----------------------------------------------------------'
 fi
--- base/rootfs/etc/cont-init.d/01-log-level.sh
+++ base/rootfs/etc/cont-init.d/01-log-level.sh
@@ -1,50 +1,47 @@
-#!/usr/bin/with-contenv bash
+#!/usr/bin/with-contenv bashio
 # ==============================================================================
 # Community Hass.io Add-ons: Base Images
 # Sets the log level correctly
 # ==============================================================================
-# shellcheck source=base/rootfs/usr/lib/hassio-addons/base.sh
-source /usr/lib/hassio-addons/base.sh
-
 declare log_level
 
 # Check if the log level configuration option exists
-if hass.config.exists "log_level"; then
+if bashio::config.exists log_level; then
 
     # Find the matching LOG_LEVEL
-    case "$(hass.string.lower "$(hass.config.get 'log_level')")" in
+    case "$(bashio::string.lower "$(bashio::config log_level)")" in
         all)
-            log_level="${LOG_LEVEL_ALL}"
+            log_level="${__BASHIO_LOG_LEVEL_ALL}"
             ;;
         trace)
-            log_level="${LOG_LEVEL_TRACE}"
+            log_level="${__BASHIO_LOG_LEVEL_TRACE}"
             ;;
         debug)
-            log_level="${LOG_LEVEL_DEBUG}"
+            log_level="${__BASHIO_LOG_LEVEL_DEBUG}"
             ;;
         info)
-            log_level="${LOG_LEVEL_INFO}"
+            log_level="${__BASHIO_LOG_LEVEL_INFO}"
             ;;
         notice)
-            log_level="${LOG_LEVEL_NOTICE}"
+            log_level="${__BASHIO_LOG_LEVEL_NOTICE}"
             ;;
         warning)
-            log_level="${LOG_LEVEL_WARNING}"
+            log_level="${__BASHIO_LOG_LEVEL_WARNING}"
             ;;
         error)
-            log_level="${LOG_LEVEL_ERROR}"
+            log_level="${__BASHIO_LOG_LEVEL_ERROR}"
             ;;
         fatal)
-            log_level="${LOG_LEVEL_FATAL}"
+            log_level="${__BASHIO_LOG_LEVEL_FATAL}"
             ;;
         off)
-            log_level="${LOG_LEVEL_OFF}"
+            log_level="${__BASHIO_LOG_LEVEL_OFF}"
             ;;
         *)
-            hass.die "Unknown log_level: ${log_level}"
+            bashio::exit.nok "Unknown log_level: ${log_level}"
     esac
 
     # Save determined log level so S6 can pick it up later
     echo "${log_level}" > /var/run/s6/container_environment/LOG_LEVEL
-    echo "Log level is set to ${LOG_LEVELS[$log_level]}"
+    bashio::log.blue "Log level is set to ${__BASHIO_LOG_LEVELS[$log_level]}"
 fi
